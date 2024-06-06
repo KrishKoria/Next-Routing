eb44630e833b6d0d13a48ba0d9009f43fd652295
KrishKoria <krishkoria2004@gmail.com>
2024-06-06 16:52:32 +0530

    Created Initial app

---
+++ app/news/[id]/page.js
@@ -0,0 +1,21 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import Image from "next/image";
+
+export default function NewsDetailsPage({ params }) {
+  const newsItem = DUMMY_NEWS.find((news) => news.slug === params.id);
+  return (
+    <article className="news-article">
+      <header>
+        <Image
+          src={`/images/news/${newsItem.image}`}
+          alt={newsItem.title}
+          width={112}
+          height={112}
+        />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  );
+}
