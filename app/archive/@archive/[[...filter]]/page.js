cb69d51ee2705ced75f76f8a99fac552dcd696db
KrishKoria <krishkoria2004@gmail.com>
2024-06-07 21:32:52 +0530

    Replace archive by catch-all routes

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,55 @@
+import { NewsList } from "@/components/news_list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
+import Link from "next/link";
+
+export default function FilteredNewsPage({ params }) {
+  const selectedYear = params.filter?.[0];
+  const selectedMonth = params.filter?.[1];
+  let links = getAvailableNewsYears();
+  let news;
+  let newsContent = <p>No News Found for the Selected Year</p>;
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid Year or Month");
+  }
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import { NewsList } from "@/components/news_list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-  const year = params.year;
-  const news = getNewsForYear(year);
-  return <NewsList news={news} />;
-}
