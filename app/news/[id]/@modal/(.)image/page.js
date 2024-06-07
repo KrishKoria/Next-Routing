9bcef2b4707bafdbcb6f727c231877399f870c4e
KrishKoria <krishkoria2004@gmail.com>
2024-06-07 22:25:41 +0530

    feat: Improve modal functionality in InterceptedImagePage
    The code changes in this commit improve the functionality of the modal in the InterceptedImagePage component. The modal now includes a backdrop that can be clicked to navigate back using the router. This enhances the user experience by providing an intuitive way to close the modal.

--- app/news/[id]/@modal/(.)image/page.js
+++ app/news/[id]/@modal/(.)image/page.js
@@ -1,14 +1,17 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
   const newsItem = DUMMY_NEWS.find((news) => news.slug === params.id);
   if (!newsItem) {
     notFound();
   }
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <Image
