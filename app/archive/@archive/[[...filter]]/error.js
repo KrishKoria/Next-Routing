483f1ad3a4bc23ffcba757160dd8beaa2f22749f
KrishKoria <krishkoria2004@gmail.com>
2024-06-07 21:37:51 +0530

    added error fallback

---
+++ app/archive/@archive/[[...filter]]/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function ErrorPage() {
+  return (
+    <div id="error">
+      <h1>Invalid Year or Month</h1>
+      <p>Please go back and select a valid year or month.</p>
+    </div>
+  );
+}
